f1ff9c4c78e61d1cfcd15354218690bad4bcdb42
Mary Barbie Barrion <mary.barrion@student.ateneo.edu>
2024-03-20 00:17:08 +0800

    Update views.py

--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "merchstore/product_list.html"
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "merchstore/product_detail.html"
