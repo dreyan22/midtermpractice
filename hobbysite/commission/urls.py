44b0b924ee5f07c3bbadaf9bdfc73b0523b6d486
Adrian De Guzman <adrian.deguzman@student.ateneo.edu>
2024-03-20 00:25:46 +0800

    Added commission in installed_apps and completed commission models.py and views.py,

---
+++ hobbysite/commission/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import CommissionListView, CommissionDetailView
+
+
+urlpatterns = [
+    path('commissions/list', CommissionListView.as_view(), name='commission_list'),
+    path('commission/<int:pk>', CommissionDetailView.as_view(), name='commission_detail'),
+]
+
+
+app_name = 'commission'
---
+++ hobbysite/commission/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+from .models import Commission
+
+
+class CommissionListView(ListView):
+    model = Commission
+    template_name = "commission/commission_list.html"
+
+
+class CommissionDetailView(DetailView):
+    model = Commission
+    template_name = "commission/commission_detial.html"
