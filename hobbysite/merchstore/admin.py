c81f7ca2514c5d3166f04c9a5c9aef3a23b3ad3b
Mary Barbie Barrion <mary.barrion@student.ateneo.edu>
2024-03-20 00:28:24 +0800

    created admin.py

--- hobbysite/merchstore/admin.py
+++ hobbysite/merchstore/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Product, ProductType
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'product_type', 'price')
+    search_fields = ['name']
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ['name']
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductType, ProductTypeAdmin)
