8a9c5a5c9355cd724f8503f28bfc9961a8b90df5
Mary Barbie Barrion <mary.barrion@student.ateneo.edu>
2024-03-20 02:00:41 +0800

    updates stuffs

--- hobbysite/merchstore/admin.py
+++ hobbysite/merchstore/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Product, ProductType
 
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'product_type',  'product_type_description', 'description', 'formatted_price')
     search_fields = ['name']
@@ -13,10 +14,16 @@ class ProductAdmin(admin.ModelAdmin):
         return obj.product_type.description
     product_type_description.short_description = 'Product Type Description'
 
+    def get_absolute_url(self, obj):
+        return obj.get_absolute_url()
+
 
 class ProductTypeAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ['name']
+
+    def get_absolute_url(self, obj):
+        return obj.get_absolute_url()
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductType, ProductTypeAdmin)
